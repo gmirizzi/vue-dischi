5392c2cff5d0946c439f5274c20299b111143bf6
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-24 15:06:33 +0100

    Header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="d-flex justify-content-between p-1">
+    <img src="" alt="Logo Spotify" />
+    <select name="" id=""></select>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #2e3a46ff;
+}
+</style>
