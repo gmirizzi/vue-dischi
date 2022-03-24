b47325dbad3dbf4f2114ec94b7078a94ac97cf2b
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-24 15:22:25 +0100

    creata la componente card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,25 @@
 <template>
   <main>
-    <div class="container">content</div>
+    <div class="container">
+      <SongCard />
+    </div>
   </main>
 </template>
 
 <script>
+import SongCard from "./../components/SongCard.vue";
+
 export default {
   name: "AppMain",
+  components: {
+    SongCard,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 main {
   background-color: #1e2d3bff;
+  color: white;
 }
 </style>
---
+++ src/components/SongCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>card</div>
+</template>
+
+<script>
+export default {
+  name: "SongCard",
+};
+</script>
+
+<style></style>
