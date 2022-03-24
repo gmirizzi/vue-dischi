af066da1527201c332a17ab7a1129bbdc02c9b6d
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-24 15:10:06 +0100

    main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <div class="container">content</div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  background-color: #1e2d3bff;
+}
+</style>
